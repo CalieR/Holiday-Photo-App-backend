c129575c5eff5b1373e51fbc4fc0c7b1fbbc777c
CalieR <calielrushton@gmail.com>
2019-05-15 17:24:13 +0100

    set up and tested basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ namespace :api do
+   namespace :v1 do
+     resources :users
+     resources :photos, only: [:index]
+     resources :albums, only: [:index]
+   end
+ end
+
 end
