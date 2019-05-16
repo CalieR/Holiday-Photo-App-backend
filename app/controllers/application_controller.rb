12da68835a75363316e18bf6b965a0e087ad51fe
CalieR <calielrushton@gmail.com>
2019-05-16 14:13:16 +0100

    user can log in and out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,9 @@ class ApplicationController < ActionController::API
         request.headers["Authorization"]
       end
     
+      # current_user is reserved in rails, was getting nil token error
       def get_current_user
-        byebug
+        # byebug
         token = get_token
         decoded_token = decode_token(token)
         user = User.find(decoded_token["user_id"])
