c1400aba25f2279f6afdc9dd30aedd81cc33c00b
CalieR <calielrushton@gmail.com>
2019-05-18 16:08:09 +0100

    figured out why albumUser wasn't being created

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -7,8 +7,10 @@ class Api::V1::AlbumsController < ApplicationController
 
     def create 
         album = Album.create(name: params[:name])
-        albumUser = AlbumUser.create(album: album, user: get_current_user)
-        render json: album
+        # getting nil token error at get_current_user:
+        albumUser = AlbumUser.create(album: album, user: get_current_user, admin: true)
+        render json: album 
+        # need to render album user so can refresh state with it ?
     end
 
   
